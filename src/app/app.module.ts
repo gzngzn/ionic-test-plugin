7294871be6cce7bc17ab7cc00c0d9a186cd7587b
Nikita Bukanov <n.bukanov@yclients.tech>
2022-10-18 19:56:45 +0300

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {InAppBrowser} from '@ionic-native/in-app-browser/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, InAppBrowser],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {InAppBrowser} from '@ionic-native/in-app-browser/ngx';
+
+let inAppBrowserRef;
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +9,43 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  countLoading = 1;
+  constructor(private iab: InAppBrowser) {}
 
-  constructor() {}
+  buttonClick(): void {
+    // alert('Opening InAppBrowser');
+    const inAppBrowser = this.iab.create('https://ionicframework.com/', '_blank',
+      'clearcache=yes,clearsessioncache=yes,location=yes,hardwareback=no,zoom=no');
 
+    inAppBrowser.on('loadstart').subscribe(event => {
+      if(event.url.includes('twitter')){
+        // alert('Twitter opened');
+        inAppBrowser.close();
+      }
+    });
+  }
+
+  openInAppBrowser(): void {
+    /* Open URL */
+    const openUrl = 'https://ionicframework.com/';
+    inAppBrowserRef = (window as any).cordova.InAppBrowser.open(
+      openUrl, '_blank', 'clearcache=yes,clearsessioncache=yes,location=yes,hardwareback=no,zoom=no'
+    );
+    /* Add event listener to close InAppBrowser*/
+    inAppBrowserRef.addEventListener('loadstart', this.loadStartCallBack);
+    // inAppBrowserRef.addEventListener( 'loadstop', function() {
+    //   inAppBrowserRef.executeScript({ code: 'alert( \'hello\' );' });
+    // });
+  };
+
+
+  loadStartCallBack(event): void {
+    /* Close InAppBrowser if loading the predefined close URL */
+    // alert(this.countLoading++);
+    // inAppBrowserRef.executeScript({ code: 'alert( \'hello\' );' });
+    if (event.url.includes('twitter')) {
+      // alert('twitter detected');
+      inAppBrowserRef.close();
+    }
+  }
 }
